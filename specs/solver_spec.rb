4cd688604898f93372c6d351ec70ccbea70b6ccf
Zeeshan Haider <zhadier39@gmail.com>
2022-04-14 20:30:17 +0500

    Add initial test for checking initialization of Class Solver

---
+++ specs/solver_spec.rb
@@ -0,0 +1,11 @@
+describe Solver do
+  before :each do
+    @solver = Solver.new 
+  end
+
+  context '#new' do
+    it 'Initializes Solver when 0 arguements given' do
+      expect(@solver).to be_instance_of(Solver)
+    end
+  end
+end
