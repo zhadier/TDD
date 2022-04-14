28b3f42f94a0cb2a2208d54a82107f7962f4e7c4
Zeeshan Haider <zhadier39@gmail.com>
2022-04-14 20:36:58 +0500

    add test for checking of factorial returning correct value

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -9,5 +9,15 @@ describe Solver do
     it 'Initializes Solver when 0 arguements given' do
       expect(@solver).to be_instance_of(Solver)
     end
+
+    it 'Gives error when arguements given' do
+    expect{ Solver.new(1) }.to raise_error(ArgumentError)
+    end
+  end
+
+  context '#factorial' do
+    it 'takes one arguement and returns its factorial' do
+      expect (@solver.factorial(3)).to eql(6)
+    end
   end
 end
