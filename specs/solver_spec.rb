ad016d70e69967d97abac4a0113529ffa231e5a3
lisandroseia <lisandroseia11@gmail.com>
2022-04-14 13:09:02 -0300

    Add test case for string reversor

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -1,8 +1,8 @@
-require_relative "../solver"
+require_relative '../solver'
 
 describe Solver do
   before :each do
-    @solver = Solver.new 
+    @solver = Solver.new
   end
 
   context '#new' do
@@ -11,7 +11,7 @@ describe Solver do
     end
 
     it 'Gives error when arguements given' do
-    expect{ Solver.new(1) }.to raise_error(ArgumentError)
+      expect { Solver.new(1) }.to raise_error(ArgumentError)
     end
   end
 
@@ -21,16 +21,22 @@ describe Solver do
     end
 
     it 'returns 1 if argument is 0' do
-       expect(@solver.factorial(0)).to eql(1)
+      expect(@solver.factorial(0)).to eql(1)
     end
 
     it 'raises exception if number is negative' do
-      expect{@solver.factorial(-1)}.to raise_error("Error, arguement for factorial needs to be a +ve Int type")
+      expect { @solver.factorial(-1) }.to raise_error('Error, arguement for factorial needs to be a +ve Int type')
     end
 
     it 'raises exception if number is not an Int' do
-        expect{@solver.factorial('Zeeshan')}.to raise_error("Error, arguement for factorial needs to be a +ve Int type")
+      expect do
+        @solver.factorial('Zeeshan')
+      end.to raise_error('Error, arguement for factorial needs to be a +ve Int type')
     end
-
+  end
+  context '#reverse' do
+    it 'hello should return olleh' do
+      expect(@solver.reverse('hello')).to eql('olleh')
+    end
   end
 end
